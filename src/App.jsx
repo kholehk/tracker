05d00e6fcb0d59576d95c01f106664017f2c17ee
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2021-03-10 20:44:02 +0200

    init app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container center-align">
+      <h3>tracker</h3>
+      <form action="#" className="col s12">
+        <input type="text" name="tracker" id="newTracker" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
