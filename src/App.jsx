a4edd80769fbd504e030fbe3525f5ff503ed6c5d
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2021-03-11 20:10:54 +0200

    add some style

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,36 @@ function App() {
           <i className="material-icons icon">play_circle</i>
         </button>
       </form>
+      <ul className="list">
+        <li className="item">
+          <span className="tracker-title">Lorem, ipsum dolor.</span>
+          <span className="tracker-time m1">01:15:56</span>
+          <button className="button m1" type="button">
+            <i className="material-icons icon black">
+              pause_circle_outline
+            </i>
+          </button>
+          <button className="button m1" type="button">
+            <i className="material-icons icon red">
+              remove_circle_outline
+            </i>
+          </button>
+        </li>
+        <li className="item">
+          <span className="tracker-title">Lorem ipsum dolor sit amet consectetur adipisicing.</span>
+          <span className="tracker-time m1">100:25:15</span>
+          <button className="button m1" type="button">
+            <i className="material-icons icon black">
+              play_circle_outline
+            </i>
+          </button>
+          <button className="button m1" type="button">
+            <i className="material-icons icon red">
+              remove_circle_outline
+            </i>
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
